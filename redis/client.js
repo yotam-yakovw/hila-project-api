d2ea595b07fbb648473c7d71694ac81e33c3c210
yotam-yakov <yotamyakov.w@gmail.com>
2024-09-04 01:02:34 +0300

    basic redis implement

--- redis/client.js
+++ redis/client.js
@@ -1,3 +1,19 @@
 const { createClient } = require('redis');
+const { REDIS_PW, REDIS_HOST, REDIS_PORT } = process.env;
 
-const client = createClient;
+const client = createClient({
+  password: REDIS_PW,
+  socket: {
+    host: REDIS_HOST,
+    port: REDIS_PORT,
+  },
+});
+
+client.on('error', (err) => console.log('Redis Error: ', err));
+
+if (!client.isOpen) {
+  client.connect();
+  console.log('Redis client connected');
+}
+
+module.exports = client;
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const client = require('../redis/client');
 
 router.get('/', (req, res) => {
   res.send('Hello User: ' + req.body.user);
+  client.set('user', req.body.user);
 });
 
 module.exports = router;
