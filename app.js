d9827d500ad53b420d1eaede8d09c6d91294572b
yotam-yakov <yotamyakov.w@gmail.com>
2024-09-06 13:49:26 +0300

    added helmet and logger middleware

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
+//Libraries
 const express = require('express');
 const cors = require('cors');
+const helmet = require('helmet');
+// Routes
 const users = require('./routes/users');
 const workplaces = require('./routes/workplaces');
+// Middleware
+const { requestLogger, errorLogger } = require('./middleware/logger');
 
 const { PORT } = process.env;
 
 app = express();
 
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(requestLogger);
 app.use('/users', users);
 app.use('/workplaces', workplaces);
-
-app.get('/', (req, res) => {
-  res.send('Test Text');
+app.use('*', (req, res) => {
+  res.status(404).send('Page does not exist!');
 });
+app.use(errorLogger);
 
 app.listen(PORT, () => {
   console.log(`Port: ${PORT}`);
